75134207ccabcb1241159cbd30f817c0f4962d52
askaydevs <sk_singh18@outlook.com>
2019-09-18 11:19:29 +0530

    Forked UnsupervisedQA, Unsupervised Neural Machine Translation (unmt) and, fastText from https://github.com/facebookresearch/UnsupervisedQA

--- logic_field.py
+++ logic_field.py
@@ -8,7 +8,7 @@ text = '\n\n'.join(text)
 print(process(text))
 '''
 
-
+'''
 import slate
 def extract_text_from_pdf(pdf_path):
     with open(pdf_path) as fh:
@@ -17,3 +17,14 @@ def extract_text_from_pdf(pdf_path):
         print(page)
 
 extract_text_from_pdf('Solverminds_Data/Sample/004.pdf')
+'''
+
+def pdf_to_text(filepath):
+    print('Getting text content for {}...'.format(filepath))
+    process = subprocess.Popen(['pdf2txt.py', filepath], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout, stderr = process.communicate()
+
+    if process.returncode != 0 or stderr:
+        raise OSError('Executing the command for {} caused an error:\nCode: {}\nOutput: {}\nError: {}'.format(filepath, process.returncode, stdout, stderr))
+
+    return stdout.decode('utf-8')
